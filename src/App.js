1811c7bd2f1e12dc75e7e436e5a56c5d583f7643
amolk4games <amolk4games@gmail.com>
2021-02-19 22:21:03 +0530

    Changes for the React application

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import  FirstComponent from './components/example-components/FirstComponent';
+import  SecondComponent from './components/example-components/SecondComponent';
+import  Counter from './components/counter/Counter';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter/>
+      </div>
+    );
+  }
+}
+
+class ExampleComponent extends Component{
+  render() {
+    return (
+      <div className="example">
+        My hello React App!
+        <FirstComponent/>
+        <SecondComponent/>
+      </div>
+    );
+  }
+}
+export default App;
